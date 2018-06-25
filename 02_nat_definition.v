eb03a914cc0fb8827183587ec7287e5319ffbf33
B-Barsik <yaroslav.ivashkevich@yandex.ru>
2018-06-25 21:44:40 +0300

    Y2

--- 02_nat_definition.v
+++ 02_nat_definition.v
@@ -3,10 +3,16 @@
 
 Module NatNumbers.
 
-Inductive nat : Type :=
+Inductive nat : Set :=
 | O : nat
 | S : nat -> nat
 .
+
+Check nat.
+Check O.
+Check S.
+Check (S O).
+Check (S (S O)).
 
 Definition pred (n : nat) : nat :=
 match n with
@@ -17,6 +23,8 @@ end.
 Compute pred (S (S (S (S O)))).
   (* = S (S (S O))
      : nat *)
+Compute pred O.
+
 
 End NatNumbers.
 
@@ -28,8 +36,7 @@ Compute pred (S (S (S (S O)))).
 Check pred (S (S (S (S O)))).
   (* ===> 4 : nat *)
 
-Print NatNumbers.nat.
-
+Print NatNumbers.pred.
 
 Definition minustwo (n : nat) : nat :=
 match n with
@@ -38,7 +45,9 @@ match n with
 | S (S m) => m
 end.
 
-Compute (minustwo 4).
+Check minustwo.
+
+Compute (minustwo 5).
   (* ===> 2 : nat *)
 
 
@@ -53,24 +62,22 @@ match n with
 | S (S m) 	=> evenb m
 end.
 
-Definition oddb (n : nat) : bool   :=   negb (evenb n).
+Definition oddb (n : nat) : bool := negb (evenb n).
 
 Example test_oddb1 : 
   oddb 1 = true.
 Proof. 
-  simpl. 
   reflexivity.
 Qed.
 Example test_oddb2 :
   oddb 4 = false.
-Proof. 
-  simpl.
+Proof.
   reflexivity.
 Qed.
 
 Module NatNumbers2.
 
-Fixpoint plus (n : nat) (m : nat) : nat :=
+Fixpoint plus n m :=
 match n with
 | O => m
 | S k => S (plus k m)
@@ -87,7 +94,6 @@ end.
 Example test_mult1 : 
   (mult 3 3) = 9.
 Proof.
-  simpl.
   reflexivity.
 Qed.
 
@@ -100,25 +106,33 @@ end.
 
 End NatNumbers2.
 
-Fixpoint exp (base power : nat) : nat :=
-match power with
+Fixpoint exp (n m : nat) : nat :=
+match m with
 | O => S O
-| S p => mult base (exp base p)
+| S p => mult n (exp n p)
 end.
 
-(*
+
 Fixpoint factorial (n : nat) : nat :=
   (* написать определени факторила *)
+match n with
+| O => S O
+| S p => mult n (factorial p)
+end.
+
+
 
 Example test_factorial1 :
   (factorial 3) = (plus 2 4).
-(* проверить свою функцию *)
+Proof. 
+  reflexivity. 
+Qed.
 
 Example test_factorial2 :
   (factorial 5) = (mult 10 12).
-(* проверить свою функцию *)
-
-*)
+Proof. 
+  reflexivity. 
+Qed.
 
 Notation "x + y" := (plus x y)
                        (at level 50, left associativity)
@@ -130,15 +144,17 @@ Notation "x * y" := (mult x y)
                        (at level 40, left associativity)
                        : nat_scope.
 
+Compute (2 + 3 * 4).
+
 Check ((0 + 1) + 1).
 
 
 Fixpoint beq_nat (n m : nat) : bool :=
   match n with
-  | O 	    => 	match m with
-				| O => true
-         		| S m' => false
-         		end
+  | O 	  => 	match m with
+				      | O => true
+         		  | S m' => false
+         		  end
   | S n'	=>	match m with
             	| O => false
             	| S m' => beq_nat n' m'
@@ -149,9 +165,9 @@ Fixpoint leb (n m : nat) : bool :=
 match n with
 | O => true
 | S n' =>	match m with
-     		| O => false
+     		  | O => false
       		| S m' => leb n' m'
-      	    end
+      	  end
 end.
 
 Example test_leb1 :
@@ -212,7 +228,8 @@ Proof.
 Qed.
 
 Theorem mult_0_l : 
-  forall n:nat, 0 * n = 0.
+  forall n:nat, 
+  0 * n = 0.
 Proof.
   intros n. 
   reflexivity.  
@@ -223,14 +240,12 @@ Theorem plus_id_example :
   n = m ->
   n + n = m + m.
 Proof.
-  intros n m.
-  intros H.
-  rewrite -> H.
+  intros * H.
+  rewrite H.
   reflexivity.  
 Qed.
 
 
-(*
 (** Упраженение  *)
 
 Theorem plus_id_exercise : 
@@ -239,7 +254,11 @@ Theorem plus_id_exercise :
   m = o -> 
   n + m = m + o.
 Proof.
-  (* построить доказательство *)
+intros *.
+intros H1 H2.
+rewrite H1.
+rewrite H2.
+reflexivity.
 Qed.
 
 
@@ -247,25 +266,28 @@ Theorem mult_0_plus :
   forall n m : nat,
   (0 + n) * m = n * m.
 Proof.
-  (* построить доказательство *)
+intros *.
+reflexivity.
 Qed.
 
-(** **** Exercise: 2 stars (mult_S_1)  *)
 Theorem mult_S_1 : 
   forall n m : nat,
   m = S n ->
   m * (1 + n) = m * m.
 Proof.
-  (* построить доказательство *)
+intros *.
+intros H.
+rewrite H.
+reflexivity.
 Qed.
-*)
+
 
 Theorem plus_1_neq_0_firsttry : 
   forall n : nat,
   beq_nat (n + 1) 0 = false.
 Proof.
   intros n.
-  simpl.  (* does nothing! *)
+  simpl.
 Abort.
 
 Theorem plus_1_neq_0 : 
@@ -273,7 +295,7 @@ Theorem plus_1_neq_0 :
   beq_nat (n + 1) 0 = false.
 Proof.
   intros n. 
-  destruct n as [| n'].
+  destruct n as [| m ].
   - reflexivity.
   - reflexivity.
 Qed.
@@ -293,14 +315,14 @@ Theorem andb_commutative :
   forall b c, 
   andb b c = andb c b.
 Proof.
-  intros b c. 
-  destruct b.
-  - destruct c.
-    + reflexivity.
-    + reflexivity.
-  - destruct c.
-    + reflexivity.
-    + reflexivity.
+    intros b c. 
+    destruct b.
+    - destruct c.
+      + reflexivity.
+      + reflexivity.
+    - destruct c.
+      + reflexivity.
+      + reflexivity.
 Qed.
 
 Theorem andb_commutative' : 
@@ -344,7 +366,7 @@ Theorem plus_1_neq_0' :
   forall n : nat,
   beq_nat (n + 1) 0 = false.
 Proof.
-  intros [|n].
+  intros [| n ].
   - reflexivity.
   - reflexivity.  
 Qed.
@@ -362,29 +384,26 @@ Qed.
 
 (** Упражнение *)
 
-(*
-
 Theorem andb_true_elim2 : 
   forall b c : bool,
   andb b c = true -> 
   c = true.
 Proof.
-  (* построить доказательство *)
+intros [] [] H; rewrite <- H; reflexivity.
 Qed.
-(** [] *)
 
-(** **** Exercise: 1 star (zero_nbeq_plus_1)  *)
 Theorem zero_nbeq_plus_1 : 
   forall n : nat,
   beq_nat 0 (n + 1) = false.
 Proof.
-  (* построить доказательство *)
+  intros n. 
+  destruct n as [| m ].
+  - reflexivity.
+  - reflexivity.
 Qed.
-*)
-
-
 
 (* пример неудачной функции *)
+
 (*
 Fixpoint plusx (n m : nat) : nat :=
   match n with
@@ -393,7 +412,6 @@ Fixpoint plusx (n m : nat) : nat :=
   end.
 *)
 
-
 (** Упражнения *)
 
 Theorem identity_fn_applied_twice :
@@ -401,21 +419,29 @@ Theorem identity_fn_applied_twice :
   (forall (x : bool), f x = x) ->
   forall (b : bool), f (f b) = b.
 Proof.
-  (* построить доказательство *)
-Admitted.
+  intros * H b.
+  repeat rewrite (H b). 
+  reflexivity.
+Qed.
 
 Theorem negation_fn_applied_twice :
   forall (f : bool -> bool),
   (forall (x : bool), f x = negb x) ->
   forall (b : bool), f (f b) = b.
 Proof.
+  intros * H1 b.
+  rewrite (H1 b).
+  rewrite (H1 (negb b)).
+  case b; reflexivity.
   (* построить доказательство *)
-Admitted.
-
+Qed.
 Theorem andb_eq_orb :
   forall (b c : bool),
   (andb b c = orb b c) ->
   b = c.
 Proof.
+  intros * H.
+  case b, c; simpl in H;try reflexivity; rewrite H; reflexivity.
+
   (* построить доказательство *)
-Admitted.
+Qed.
