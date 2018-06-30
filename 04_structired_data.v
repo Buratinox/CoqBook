c66345570b65846cdd6624436b120fe48b34e847
Buratinox <40119489+Buratinox@users.noreply.github.com>
2018-06-30 15:19:28 +0300

    P1

--- 04_structired_data.v
+++ 04_structired_data.v
@@ -1,24 +1,9 @@
-(** *  Структурные данные *)
-
-(** Пары булевых переменных *)
-
-Inductive boolprod : Type :=
-| pairb : bool -> bool -> boolprod.
-
-Definition fstb (p : boolprod) : bool :=
-  match p with
-  | pairb x y => x
-  end.
-
-Definition sndb (p : boolprod) : bool :=
-  match p with
-  | pairb x y => y
-  end.
-
-(** * Пары натуральных чисел *)
+(** Structured Data *)
 
 Inductive natprod : Type :=
 | pair : nat -> nat -> natprod.
+
+Check (pair 3 5).
 
 Definition fst (p : natprod) : nat :=
   match p with
@@ -54,8 +39,7 @@ Definition swap_pair (p : natprod) : natprod :=
 Theorem surjective_pairing' : forall (n m : nat),
   (n,m) = (fst (n,m), snd (n,m)).
 Proof.
-  reflexivity.
-Qed.
+  reflexivity.  Qed.
 
 Theorem surjective_pairing_stuck : forall (p : natprod),
   p = (fst p, snd p).
@@ -66,18 +50,254 @@ Abort.
 Theorem surjective_pairing : forall (p : natprod),
   p = (fst p, snd p).
 Proof.
-  intros p.
-  destruct p as [n m].
-  simpl.
-  reflexivity.
-Qed.
+  intros p.  destruct p as [n m].  simpl.  reflexivity.  Qed.
 
 Theorem snd_fst_is_swap : forall (p : natprod),
   (snd p, fst p) = swap_pair p.
 Proof.
-Admitted.
+  (*  *) Admitted.
 
+(** **** Exercise: 1 star, optional (fst_swap_is_snd)  *)
 Theorem fst_swap_is_snd : forall (p : natprod),
   fst (swap_pair p) = snd p.
 Proof.
-Admitted.
+  (*  *) Admitted.
+
+(** * Lists of Numbers *)
+
+Inductive natlist : Type :=
+  | nil  : natlist
+  | cons : nat -> natlist -> natlist.
+
+
+Definition mylist := cons 1 (cons 2 (cons 3 nil)).
+
+Notation "x :: l" := (cons x l)
+                     (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
+Definition mylist1 := 1 :: (2 :: (3 :: nil)).
+Definition mylist2 := 1 :: 2 :: 3 :: nil.
+Definition mylist3 := [1;2;3].
+
+Fixpoint repeat (n count : nat) : natlist :=
+  match count with
+  | O => nil
+  | S count' => n :: (repeat n count')
+  end.
+
+Fixpoint length (l:natlist) : nat :=
+  match l with
+  | nil => O
+  | h :: t => S (length t)
+  end.
+
+Fixpoint app (l1 l2 : natlist) : natlist :=
+  match l1 with
+  | nil    => l2
+  | h :: t => h :: (app t l2)
+  end.
+
+Notation "x ++ y" := (app x y)
+                     (right associativity, at level 60).
+
+Example test_app1:             [1;2;3] ++ [4;5] = [1;2;3;4;5].
+Proof. reflexivity.  Qed.
+Example test_app2:             nil ++ [4;5] = [4;5].
+Proof. reflexivity.  Qed.
+Example test_app3:             [1;2;3] ++ nil = [1;2;3].
+Proof. reflexivity.  Qed.
+
+Definition hd (default:nat) (l:natlist) : nat :=
+  match l with
+  | nil => default
+  | h :: t => h
+  end.
+
+Definition tl (l:natlist) : natlist :=
+  match l with
+  | nil => nil
+  | h :: t => t
+  end.
+
+Example test_hd1:             hd 0 [1;2;3] = 1.
+Proof. reflexivity.  Qed.
+Example test_hd2:             hd 0 [] = 0.
+Proof. reflexivity.  Qed.
+Example test_tl:              tl [1;2;3] = [2;3].
+Proof. reflexivity.  Qed.
+
+
+(* ----------------------------------------------------------------- *)
+(** *** Exercises *)
+
+Fixpoint nonzeros (l:natlist) : natlist
+  (* *). Admitted.
+
+Example test_nonzeros:
+  nonzeros [0;1;0;2;3;0;0] = [1;2;3].
+
+
+Fixpoint oddmembers (l:natlist) : natlist
+  (* *). Admitted.
+
+Example test_oddmembers:
+  oddmembers [0;1;0;2;3;0;0] = [1;3].
+  (* *) Admitted.
+
+
+Definition countoddmembers (l:natlist) : nat
+  (* *). Admitted.
+
+Example test_countoddmembers1:
+  countoddmembers [1;0;3;1;4;5] = 4.
+  (* *) Admitted.
+
+Example test_countoddmembers2:
+  countoddmembers [0;2;4] = 0.
+  (* *) Admitted.
+
+Example test_countoddmembers3:
+  countoddmembers nil = 0.
+  (* *) Admitted.
+
+Fixpoint alternate (l1 l2 : natlist) : natlist.
+  (* *) Admitted.
+
+Example test_alternate1:
+  alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
+  (* *) Admitted.
+
+Example test_alternate2:
+  alternate [1] [4;5;6] = [1;4;5;6].
+  (* *) Admitted.
+
+Example test_alternate3:
+  alternate [1;2;3] [4] = [1;4;2;3].
+  (* *) Admitted.
+
+Example test_alternate4:
+  alternate [] [20;30] = [20;30].
+  (* *) Admitted.
+
+Theorem nil_app : forall l:natlist,
+  [] ++ l = l.
+Proof. reflexivity. Qed.
+
+Theorem tl_length_pred : forall l:natlist,
+  pred (length l) = length (tl l).
+Proof.
+  intros l. destruct l as [| n l'].
+  - (* l = nil *)
+    reflexivity.
+  - (* l = cons n l' *)
+    reflexivity.  Qed.
+
+Theorem app_assoc : forall l1 l2 l3 : natlist,
+  (l1 ++ l2) ++ l3 = l1 ++ (l2 ++ l3).
+Proof.
+  intros l1 l2 l3. induction l1 as [| n l1' IHl1'].
+  - (* l1 = nil *)
+    reflexivity.
+  - (* l1 = cons n l1' *)
+    simpl. rewrite -> IHl1'. reflexivity.  Qed.
+
+Fixpoint rev (l:natlist) : natlist :=
+  match l with
+  | nil    => nil
+  | h :: t => rev t ++ [h]
+  end.
+
+Example test_rev1:            rev [1;2;3] = [3;2;1].
+Proof. reflexivity.  Qed.
+Example test_rev2:            rev nil = nil.
+Proof. reflexivity.  Qed.
+
+
+Theorem rev_length_firsttry : forall l : natlist,
+  length (rev l) = length l.
+Proof.
+  intros l. induction l as [| n l' IHl'].
+  - (* l = [] *)
+    reflexivity.
+  - (* l = n :: l' *)
+    simpl.
+    rewrite <- IHl'.
+Abort.
+
+Theorem app_length : forall l1 l2 : natlist,
+  length (l1 ++ l2) = (length l1) + (length l2).
+Proof.
+  (* WORKED IN CLASS *)
+  intros l1 l2. induction l1 as [| n l1' IHl1'].
+  - (* l1 = nil *)
+    reflexivity.
+  - (* l1 = cons *)
+    simpl. rewrite -> IHl1'. reflexivity.  Qed.
+
+
+Theorem rev_length : forall l : natlist,
+  length (rev l) = length l.
+Proof.
+  intros l. induction l as [| n l' IHl'].
+  - (* l = nil *)
+    reflexivity.
+  - (* l = cons *)
+    simpl. rewrite -> app_length, plus_comm.
+    simpl. rewrite -> IHl'. reflexivity.  Qed.
+
+Theorem app_nil_r : forall l : natlist,
+  l ++ [] = l.
+Proof.
+  (* *) Admitted.
+
+Theorem rev_app_distr: forall l1 l2 : natlist,
+  rev (l1 ++ l2) = rev l2 ++ rev l1.
+Proof.
+  (* *) Admitted.
+
+Theorem rev_involutive : forall l : natlist,
+  rev (rev l) = l.
+Proof.
+  (* *) Admitted.
+
+Theorem app_assoc4 : forall l1 l2 l3 l4 : natlist,
+  l1 ++ (l2 ++ (l3 ++ l4)) = ((l1 ++ l2) ++ l3) ++ l4.
+Proof.
+  (* *) Admitted.
+
+Lemma nonzeros_app : forall l1 l2 : natlist,
+  nonzeros (l1 ++ l2) = (nonzeros l1) ++ (nonzeros l2).
+Proof.
+  (* *) Admitted.
+
+Fixpoint beq_natlist (l1 l2 : natlist) : bool.
+  (* *) Admitted.
+
+Example test_beq_natlist1 :
+  (beq_natlist nil nil = true).
+  (* *) Admitted.
+
+Example test_beq_natlist2 :
+  beq_natlist [1;2;3] [1;2;3] = true.
+  (* *) Admitted.
+
+Example test_beq_natlist3 :
+  beq_natlist [1;2;3] [1;2;4] = false.
+  (* *) Admitted.
+
+Theorem beq_natlist_refl : forall l:natlist,
+  true = beq_natlist l l.
+Proof.
+  (* *) Admitted.
+
+Theorem ble_n_Sn : forall n,
+  leb n (S n) = true.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - (* 0 *)
+    simpl.  reflexivity.
+  - (* S n' *)
+    simpl.  rewrite IHn'.  reflexivity.  
+Qed.
